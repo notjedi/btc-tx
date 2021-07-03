1408bb0ea07660e08586f24aa990464479938214
jedi2610 <krithickumarub@protonmail.com>
2021-07-03 16:22:02 +0530

    minor refactors

--- tx.py
+++ tx.py
@@ -4,7 +4,7 @@ import ecdsa
 
 from ecdsa import util
 from wallet import generate_key_pair
-from utils import hexify, toLittleEndian, sha256
+from utils import hexify, toLittleEndian, sha256, getLen
 
 
 def makeScriptPubKey(addr):
@@ -19,14 +19,10 @@ def makeScriptPubKey(addr):
             'ac') # OP_CHECKSIG
 
 
-def getLenOfHex(val):
-    return hexify(len(val) // 2)
-
-
 def makeOutput(data):
     value, addr = data
     scriptPubKey = makeScriptPubKey(addr)
-    scriptPubKeySize = getLenOfHex(scriptPubKey)
+    scriptPubKeySize = getLen(scriptPubKey)
     return hexify(struct.pack('<Q', value)) + scriptPubKeySize + scriptPubKey
 
 
@@ -35,14 +31,14 @@ def makeRawTx(txid, vout, scriptSig, outputs):
     # https://learnmeabitcoin.com/technical/transaction-data
     # https://bitcoin.stackexchange.com/questions/35878/is-there-a-maximum-size-of-a-scriptsig-scriptpubkey
     # https://www.blockchain.com/btc-testnet/tx/dc2a7fa88c93327fe70893df86d1ed9df4904c8a586d661895756a7b528fbe01
-    version = toLittleEndian('00000001') # version: 1
+    version = toLittleEndian(1) # version: 1
     lockTime = '00000000'
 
     # inputs
     input_count = '01' # total input count
     txid = toLittleEndian(txid) # txid (hash of the last tx)
     vout = toLittleEndian(vout) # index of the output from the last tx
-    inputs = input_count + txid + vout + hexify(len(scriptSig)) + scriptSig + 'ffffffff'
+    inputs = input_count + txid + vout + getLen(scriptSig) + scriptSig + 'ffffffff'
     # outputs
     output_count = hexify(len(outputs))
     outputs = ''.join(map(makeOutput, outputs))
@@ -64,18 +60,16 @@ def makeSignedTx(output_hash, index, outputs):
 
     # tx without scriptSig
     scriptPubKey = makeScriptPubKey(pub_key_hash)
-    scriptPubKeySize = getLenOfHex(scriptPubKey)
-    rawTx = makeRawTx(output_hash, index, scriptPubKeySize + scriptPubKey, outputs)
+    rawTx = makeRawTx(output_hash, index, scriptPubKey, outputs) # TODO: add hash code
     # sign rawTx
     txDigest = sha256(sha256(rawTx))
     signedTx = signTx(txDigest)
     # tx with scriptSig
-    scriptSig = getLenOfHex(signedTx) + hexify(signedTx) + getLenOfHex(pub_key) + pub_key
+    scriptSig = getLen(signedTx) + hexify(signedTx) + getLen(pub_key) + pub_key
     return makeRawTx(output_hash, index, scriptSig, outputs)
 
 
 if __name__ == '__main__':
     random.seed(3301)
     priv_key, wif, pub_key, pub_key_hash, pub_addr = generate_key_pair()
-    
     print('pub_addr:', pub_addr)
--- utils.py
+++ utils.py
@@ -3,13 +3,28 @@ import base58
 import binascii
 import struct
 
+def btcToSatoshi(val):
+    # 1 BTC = 1e8 satoshi
+    return val * 1e8
+
 def toLittleEndian(val):
     # big endian to little endian
-    # return bytearray.fromhex(val).reverse()
-    return hexify(struct.pack('<L', val))
+    if isinstance(val, bytes):
+        out = struct.pack('<L', val)
+    else:
+        out = bytearray.fromhex(val)
+        out.reverse()
+    return hexify(out)
 
 def hexify(val):
-    return binascii.hexlify(val).decode()
+    if isinstance(val, bytes) or isinstance(val, bytearray):
+        return binascii.hexlify(val).decode()
+    else:
+        return hex(val)[2:]
+
+def getLen(val):
+    # gets the length of the hex value given
+    return hexify(len(binascii.unhexlify(val)))
 
 def sha256(val):
     return hashlib.sha256(val).digest()
